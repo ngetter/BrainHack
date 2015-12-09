81dc0599a31a1f5f8df0dd8cb3359f23b6deb2d4
bigShai <shai@bizzabo.com>
2015-12-09 20:29:56 +0200

    4

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/data', function (req, res) {
 
 });
 
-var server = app.listen(8000, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
 
